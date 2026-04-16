afe679e1062013b7319f0b3c62debf869f4ae8ce
GaurishBhaskar <learn.gaurishbhaskar@gmail.com>
2026-04-16 09:49:44 +0530

    Add configuration for Git commit message validator

---
+++ js/script.js
@@ -0,0 +1,23 @@
+{
+  "_comment": "Git Commit Message Validator — Configuration File",
+  "_docs": "https://github.com/yourusername/git-commit-message-validator#configuration",
+
+  "commit_types": [
+    "feat",
+    "fix",
+    "docs",
+    "style",
+    "refactor",
+    "test",
+    "chore",
+    "perf",
+    "ci",
+    "build"
+  ],
+
+  "max_subject_length": 100,
+  "min_subject_length": 5,
+
+  "_custom_example": "Add your own types like 'wip', 'hotfix', 'release' below:",
+  "_custom_types_example": ["wip", "hotfix", "release", "revert"]
+}
